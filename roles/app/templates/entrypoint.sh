5d7220365084600957d3449519f979b9a5f23b21
Vibin Daniel <vibind@drivevariant.com>
2021-02-17 15:33:58 -0500

    Fix entrypoint.sh

--- roles/app/templates/entrypoint.sh
+++ roles/app/templates/entrypoint.sh
@@ -6,7 +6,7 @@ PYTHON_MANAGER="{{ python.manager }}"
 if [ "$PYTHON_MANAGER" == "conda" ]; then
   # shellcheck disable=SC1091
   source "/opt/conda3/etc/profile.d/conda.sh"
-  conda activate
+  conda activate "{{ python.env_name }}"
 fi
 if [ "$PYTHON_MANAGER" == "pip" ]; then
   # shellcheck disable=SC1091
@@ -20,16 +20,13 @@ touch "/home/{{ instance_user }}/logs/{{ type }}/{{ name }}.error.log"
 # trap handler: print location of last error and process it further
 function handle_err() {
   MYSELF="$0"   # equals to my script name
-  LASTLINE="$1" # argument 1: last line of error occurence
-  LASTERR="$2"  # argument 2: error code of last command
+  LASTERR="$1"  # argument 1: error code of last command
   echo -e "${MYSELF}: line ${LASTLINE}\n${BASH_COMMAND}\nexit status of command: ${LASTERR}" \
     >>"/home/{{ instance_user }}/logs/{{ type }}/{{ name }}.error.log"
 }
 
 # trap commands with non-zero exit code
-trap 'handle_err ${LINENO} $?' ERR
+trap 'handle_err $?' ERR
 
-rm "/home/{{ instance_user }}/logs/{{ type }}/{{ name }}.log" || true
 # shellcheck disable=SC1083
-bash -c "cd "/home/{{ instance_user }}/app" && {{ entrypoint }}" \
-  >>"/home/{{ instance_user }}/logs/{{ type }}/{{ name }}.log"
+bash -c "cd /home/{{ instance_user }}/app && ({{ entrypoint }}) >/home/{{ instance_user }}/logs/{{ type }}/{{ name }}.log"
