55607c5f4bdc89bceac6ba5a407369124f561b9b
Vibin Daniel <vibind@drivevariant.com>
2021-02-17 15:40:48 -0500

    Add bucket folder for s3 backups

--- roles/app/templates/backup.sh
+++ roles/app/templates/backup.sh
@@ -8,9 +8,9 @@ inotifywait -e modify,moved_from,create,delete --timefmt '%d/%m/%y %H:%M' -m --f
 "{{ dir }}" | while read event date time fname file; do
   echo "INFO: Event:${event}: At ${time} on ${date}: file ${fname}"
   if [ "$event" == "CREATE" ] || [ "$event" == "MODIFY" ]; then
-    aws s3 cp "$file" "s3://{{ bucket_name }}$file"
+    aws s3 cp "$file" "s3://{{ bucket_name }}/{{bucket_folder}}$file"
   fi
   if [ "$event" == "DELETE" ] || [ "$event" == "MOVED_FROM" ]; then
-    aws s3api delete-object --bucket "{{ bucket_name }}" --key "${file:1}"
+    aws s3api delete-object --bucket "{{ bucket_name }}" --key "{{bucket_folder}}$file"
   fi
 done
