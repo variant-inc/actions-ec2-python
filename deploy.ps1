7e0da53c902c4f2395ca53b2594385bd4c4fb009
Vibin Daniel <vibind@drivevariant.com>
2021-02-17 12:19:58 -0500

    Rename all.yaml

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,8 @@
 Write-Information "Creating SSH Key"
 "y" | ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -P """"
 
+Rename-Item -Path group_vars/all.yaml -NewName all
+
 ce ansible-galaxy collection install ansible.posix
 ce ansible-galaxy collection install amazon.aws
 
