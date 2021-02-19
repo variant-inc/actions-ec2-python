5fc827db97a95e249c8b8ec7bde7cf197480e9fc
Vibin Daniel <vibind@drivevariant.com>
2021-02-19 13:49:09 -0500

    Add new backup.sh

--- roles/app/templates/backup.sh
+++ roles/app/templates/backup.sh
@@ -5,11 +5,11 @@ set -u
 FILE_PATH=$(realpath "{{ dir }}")
 echo "Setting up watches for $FILE_PATH"
 # shellcheck disable=SC2162
-inotifywait -e modify,moved_from,create,delete --timefmt '%d/%m/%y %H:%M' -m --format '%e %T %f %w%f' \
+inotifywait -e modify,moved_from,create,delete,moved_to --timefmt '%d/%m/%y %H:%M' -m --format '%e %T %f %w%f' \
 "$FILE_PATH" | while read event date time fname file; do
   echo "INFO: Event:${event}: At ${time} on ${date}: file ${fname}"
-  if [ "$event" == "CREATE" ] || [ "$event" == "MODIFY" ]; then
-    aws s3 cp "$file" "s3://{{ bucket_name }}/{{bucket_folder}}$fname"
+  if [ "$event" == "CREATE" ] || [ "$event" == "MODIFY" ] || [ "$event" == "MOVED_TO" ]; then
+    aws s3 cp "$file" "s3://{{ bucket_name }}/{{bucket_folder}}/$fname"
   fi
   # if [ "$event" == "DELETE" ] || [ "$event" == "MOVED_FROM" ]; then
   #   aws s3api delete-object --bucket "{{ bucket_name }}" --key "{{bucket_folder}}$file"
