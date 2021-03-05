674892c8056e3ebecd86b2d53f8c18c0d09f57de
Vibin Daniel <vibind@drivevariant.com>
2021-03-04 20:34:07 -0500

    Fix bash script to send mem usage also

--- roles/prom-metrics/templates/top.sh
+++ roles/prom-metrics/templates/top.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while sleep 1; do
+while sleep 5; do
   # shellcheck disable=SC2034
   instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id -m 1)
   instance_id=${instance_id:-instance_id}
@@ -13,9 +13,16 @@ while sleep 1; do
   echo "$public_host"
   z=$(ps aux)
   while read -r z; do
-    var=$var$(awk '{print "cpu_usage{instance_id=\"'"$instance_id"'\", node_name=\"'"$private_host"'\", external_dns=\"'"$public_host"'\", process=\""$11"\", pid=\""$2"\"}", $3z}')
+    cpu=$cpu$(awk '{print "cpu_usage{instance_id=\"'"$instance_id"'\", node_name=\"'"$private_host"'\", external_dns=\"'"$public_host"'\", process=\""$11"\", pid=\""$2"\"}", $3z}')
   done <<<"$z"
-  echo "$var"
-  curl -X POST -H "Content-Type: text/plain" --data "$var
+  echo "$cpu"
+  curl -X POST -H "Content-Type: text/plain" --data "$cpu
+  " http://localhost:9091/metrics/job/top/instance/machine
+  z=$(ps aux)
+  while read -r z; do
+    mem=$mem$(awk '{print "mem_usage{instance_id=\"'"$instance_id"'\", node_name=\"'"$private_host"'\", external_dns=\"'"$public_host"'\", process=\""$11"\", pid=\""$2"\"}", $4z}')
+  done <<<"$z"
+  echo "$mem"
+  curl -X POST -H "Content-Type: text/plain" --data "$mem
   " http://localhost:9091/metrics/job/top/instance/machine
 done
