94748499c1b4cac83bc44a8816a748303cb3a70d
Vibin Daniel <vibind@drivevariant.com>
2021-03-05 09:27:02 -0500

    fix service-monitor.yaml

--- roles/prom-metrics/templates/top.sh
+++ roles/prom-metrics/templates/top.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-app="{{ name }}"
 # shellcheck disable=SC2034
 instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id -m 1 | base64)
 instance_id=${instance_id:-=}
 # shellcheck disable=SC2034
 private_host=$(curl -s http://169.254.169.254/latest/meta-data/local-hostname -m 1 | base64)
 private_host=${private_host:-=}
-HOST="http://localhost:9091/metrics/job/top/instance@base64/$instance_id/host@base64/$private_host/app/$app"
+HOST="http://localhost:9091/metrics/job/top/instance@base64/$instance_id/host@base64/$private_host"
 
 while sleep 5; do
   cpu=""
@@ -22,7 +21,7 @@ while sleep 5; do
 
   z=$(ps axo pid,pgid,user,pcpu,pmem,cmd)
   while read -r z; do
-    var=$(awk '{printf "cpu_usage{pid=\""$1"\", pgid=\""$2"\", user=\""$3"\", cmd=\""$6; for(i=7;i<=NF;i++){printf " %s", $i} print "\"}" $5z}')
+    var=$(awk '{printf "mem_usage{pid=\""$1"\", pgid=\""$2"\", user=\""$3"\", cmd=\""$6; for(i=7;i<=NF;i++){printf " %s", $i} print "\"}" $5z}')
     var="${var//\\/\\\\\\}"
     mem="$mem$var"
   done <<<"$z"
