4d1759be8b2c96b59cbd706325a7c02397823536
Vibin Daniel <vibind@drivevariant.com>
2021-03-04 20:27:37 -0500

    Add service monitor

--- deploy.ps1
+++ deploy.ps1
@@ -5,7 +5,10 @@ Rename-Item -Path group_vars/all.yaml -NewName all
 
 ce ansible-galaxy collection install ansible.posix
 ce ansible-galaxy collection install amazon.aws
+ce ansible-galaxy collection install community.kubernetes
 
-ce pip3 install -U boto3 botocore boto
+ce pip3 install -U boto3 botocore boto openshift pyyaml kubernetes
+
+ce aws eks update-kubeconfig --name ${CLUSTER_NAME} --region ${CLUSTER_REGION}
 
 ce ansible-playbook ./site.yml -i ./aws_ec2.yml -v
